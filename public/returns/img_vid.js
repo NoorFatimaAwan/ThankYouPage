296c4768ba10c8b33265951b89682f235dfeb495
Talha Waseem <talha.waseem@phaedrasolutions.com>
2022-11-04 12:59:49 +0500

    urls

--- public/returns/img_vid.js
+++ public/returns/img_vid.js
@@ -39,7 +39,7 @@ else{
 var loadPlugin = function() {
    $.ajax({
     method: "GET",
-    url: "https://90f9-182-179-155-6.ngrok.io/orders/should_show",
+    url: "https://mcacao.phaedrasolutions.com/orders/should_show",
     data: {product_title: Shopify.checkout.line_items[0].title},
     dataType: "jsonp",
     responseType:'application/json', 
@@ -59,7 +59,7 @@ var loadPlugin = function() {
             length = Shopify.checkout.line_items.length + Shopify.checkout.line_items[x].quantity
             parent_product_id = Shopify.checkout.line_items[x].product_id
             options = `order_no=${$(".os-order-number").text().trim().split("#")[1]}&product_length=${length}&domain=${Shopify.shop}&parent_product_id=${parent_product_id}&product_id=${Shopify.checkout.line_items[x].product_id}&product_title=${Shopify.checkout.line_items[x].title}&product_image_url=${Shopify.checkout.line_items[x].image_url}`
-            container.setAttribute("data", `https://90f9-182-179-155-6.ngrok.io/orders/new?${options}`)
+            container.setAttribute("data", `https://mcacao.phaedrasolutions.com/orders/new?${options}`)
             document.getElementsByClassName("section")[0].after(body)  
           }   
         }
