4de22d3abcb17d3a165f1f53d53bbdc1391334f2
Talha Waseem <talha.waseem@phaedrasolutions.com>
2022-11-17 13:59:22 +0500

    klaviyo initial commit

---
+++ config/initializers/klaviyo.rb
@@ -0,0 +1,7 @@
+require 'klaviyo'
+
+# set your 6 digit Public API key
+Klaviyo.public_api_key = 'TwDVwr'
+
+# set your Private API key
+Klaviyo.private_api_key = 'pk_070a231bd95aee826e5c9f948dd469c066'
