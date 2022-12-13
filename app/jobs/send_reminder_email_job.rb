414eaa344a382119453e270e1b8ba94cce24dee9
Noor Fatima <noor.fatima@phaedrasolutions.com>
2022-12-13 14:21:34 +0500

    function for days

--- app/jobs/send_reminder_email_job.rb
+++ app/jobs/send_reminder_email_job.rb
@@ -6,7 +6,24 @@ class SendReminderEmailJob < ApplicationJob
   
 
   def perform(user_email,product_image_url,order_no,user_name,thank_you_page_url,order_id)
-    send_email = Order.update_email_status(order_id)
+    @order = Order.where(shop_order_id: order_id)&.last
+    send_email = false
+    if @order.created_at.to_date == Date.today
+      send_email = true
+      @order.update(email_status: 'Delivered after 1 day')
+    elsif @order.created_at.to_date + 3.days == Date.today
+      send_email = true
+      @order.update(email_status: 'Delivered after 3 days')
+    elsif @order.created_at.to_date + 4.days == Date.today
+      send_email = true
+      @order.update(email_status: 'Delivered after 4 days')
+    elsif @order.created_at.to_date + 7.days == Date.today
+      send_email = true
+      @order.update(email_status: 'Delivered after 7 days')
+    elsif @order.created_at.to_date + 14.days == Date.today
+      send_email = true
+      @order.update(email_status: 'Delivered after 14 days')
+    end
     if send_email == true
       ReminderMailer.new_reminder(user_email,product_image_url,order_no,user_name,thank_you_page_url).deliver_now
     end
--- app/models/order.rb
+++ app/models/order.rb
@@ -8,25 +8,4 @@ class Order < ApplicationRecord
     return self.images[input].variant(resize: '300x300!').processed
   end
 
-  def self.update_email_status(order_id)
-    @order = Order.where(shop_order_id: order_id)&.last
-    send_email = false
-    if @order.created_at.to_date + 1.day == Date.today
-      send_email = true
-      order.update(email_status: 'Delivered after 1 day')
-    elsif @order.created_at.to_date + 3.days == Date.today
-      send_email = true
-      order.update(email_status: 'Delivered after 3 days')
-    elsif @order.created_at.to_date + 4.days == Date.today
-      send_email = true
-      order.update(email_status: 'Delivered after 4 days')
-    elsif @order.created_at.to_date + 7.days == Date.today
-      send_email = true
-      order.update(email_status: 'Delivered after 7 days')
-    elsif @order.created_at.to_date + 14.days == Date.today
-      send_email = true
-      order.update(email_status: 'Delivered after 14 days')
-    end
-    return send_email
-  end
 end
