c65c3af40b7233ff4c7bb84a91f2be4d46fd6b75
Noor Fatima <noor.fatima@phaedrasolutions.com>
2022-12-20 20:33:34 +0500

    email sent after completion

--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -3,8 +3,8 @@ class ReminderMailer < ApplicationMailer
   def new_reminder(user_email,order_no,user_name,thank_you_page_url,sent_at_completion)
     @order_no = order_no
     @user_name = user_name
-    @thank_you_page_url = thank_you_page_url.to_s + "?open_with_mail"
     @sent_at_completion = sent_at_completion
+    @thank_you_page_url = "#{thank_you_page_url.to_s}?open_with_mail= #{sent_at_completion}"
     mail to: user_email, subject: "Reminder Email From Mcacao"
   end
 end
