609c31b845b977b39b16b90c887d382307589ea8
Noor Fatima <noor.fatima@phaedrasolutions.com>
2023-01-16 18:20:41 +0500

    rails server logs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,8 +83,11 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  Rails.logger = Logger.new(STDOUT)
-  config.logger    = ActiveSupport::Logger.new("log/#{Rails.env}.log")
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  end
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
