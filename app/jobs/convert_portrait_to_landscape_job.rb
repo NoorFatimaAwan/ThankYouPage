9631852f2c4575ac801645c4176d039b897b4e8e
Noor Fatima <noor.fatima@phaedrasolutions.com>
2023-01-17 19:24:27 +0500

    change i to index

--- app/jobs/convert_portrait_to_landscape_job.rb
+++ app/jobs/convert_portrait_to_landscape_job.rb
@@ -2,16 +2,15 @@ class ConvertPortraitToLandscapeJob < ApplicationJob
   queue_as :default
 
   def perform(order,video_count)
-
     if order.present? && video_count > 0
-      for i in 0..video_count
-        video_path = ActiveStorage::Blob.service.path_for(order&.videos[i]&.key)
-        temp_file = "#{Rails.root}/testing#{i}.mp4"
-        system( "ffmpeg -i '#{video_path}' -c copy -aspect 16:9 'testing#{i}.mp4'")
+      for index in 0..video_count
+        video_path = ActiveStorage::Blob.service.path_for(order&.videos[index]&.key)
+        temp_file = "#{Rails.root}/testing#{index}.mp4"
+        system( "ffmpeg -i '#{video_path}' -c copy -aspect 16:9 'testing#{index}.mp4'")
         if (File.exists?(temp_file))
           downloaded_video = open(temp_file)
-          order.videos.attach(io: downloaded_video  , filename: "#{order&.videos[i]&.filename}")
-          order.videos[i].purge
+          order.videos.attach(io: downloaded_video  , filename: "#{order&.videos[index]&.filename}")
+          order.videos[index].purge
           downloaded_video.close
           File.delete(temp_file)  
         end
