32f7577cd22b2395b981b42862a0869845f5453c
Noor Fatima <noor.fatima@phaedrasolutions.com>
2023-02-01 16:03:10 +0500

    blur border around videos

--- app/jobs/convert_portrait_to_landscape_job.rb
+++ app/jobs/convert_portrait_to_landscape_job.rb
@@ -6,7 +6,7 @@ class ConvertPortraitToLandscapeJob < ApplicationJob
       video_count.times do |index|
         video_path = ActiveStorage::Blob.service.path_for(order&.videos[index]&.key)
         temp_file = "#{Rails.root}/testing#{index}.mp4"
-        system( "ffmpeg -i '#{video_path}' -c copy -aspect 16:9 'testing#{index}.mp4'")
+        system(`ffmpeg -i "#{video_path}" -lavfi "color=color=black@.5:size=270x360:d=1[dark];[0:v]crop=270:360[blurbase];    [blurbase]boxblur=luma_radius='min(h,w)/20':luma_power=1:chroma_radius='min(cw,ch)/20':chroma_power=1[blurred];  [blurred][dark]overlay[darkened]; [darkened]scale=640:360[bg];    [0:v]scale=-1:360[fg]; [bg][fg]overlay=(W-w)/2:(H-h)/2" "testing#{index}.mp4"`)
         if (File.exists?(temp_file))
           downloaded_video = open(temp_file)
           order.videos.attach(io: downloaded_video  , filename: "#{order&.videos[index]&.filename}")
