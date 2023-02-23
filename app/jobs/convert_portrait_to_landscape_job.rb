90836f9b93aa3d3f01d2022b657d60d5a33d69ea
Noor Fatima <noor.fatima@phaedrasolutions.com>
2023-02-23 19:48:42 +0500

    - width and height change for grey border video

--- app/jobs/convert_portrait_to_landscape_job.rb
+++ app/jobs/convert_portrait_to_landscape_job.rb
@@ -7,8 +7,8 @@ class ConvertPortraitToLandscapeJob < ApplicationJob
         video_path = ActiveStorage::Blob.service.path_for(order&.videos[index]&.key)
         video_data = MiniExiftool.new("#{video_path}")
         if video_data&.imageheight > video_data&.imagewidth || video_data&.rotation == 90
-          temp_file = "#{Rails.root}/testing#{index}.mp4"    
-          system(`ffmpeg -i "#{video_path}" -lavfi "color=color=black@.5:size=270x360:d=1[dark];[0:v]crop=270:360[blurbase];    [blurbase]boxblur=luma_radius='min(h,w)/20':luma_power=1:chroma_radius='min(cw,ch)/20':chroma_power=1[blurred];  [blurred][dark]overlay[darkened]; [darkened]scale=640:360[bg];    [0:v]scale=-1:360[fg]; [bg][fg]overlay=(W-w)/2:(H-h)/2" "testing#{index}.mp4"`)
+          temp_file = "#{Rails.root}/testing#{index}.mp4"
+          system(`ffmpeg -i "#{video_path}" -lavfi "color=color=black@.5:size=270x360:d=1[dark];[0:v]crop=270:360[blurbase];    [blurbase]boxblur=luma_radius='min(h,w)/20':luma_power=1:chroma_radius='min(cw,ch)/20':chroma_power=1[blurred];  [blurred][dark]overlay[darkened]; [darkened]scale=720:700[bg];    [0:v]scale=-1:700[fg]; [bg][fg]overlay=(W-w)/2:(H-h)/2" "testing#{index}.mp4"`)
           if (File.exists?(temp_file))
             downloaded_video = open(temp_file)
             order.videos.attach(io: downloaded_video  , filename: "#{order&.videos[index]&.filename}")
