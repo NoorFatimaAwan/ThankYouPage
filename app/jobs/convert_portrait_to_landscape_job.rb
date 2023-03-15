7073f2098e9fbb64b0e9d7aface882b119747480
Noor Fatima <noor.fatima@phaedrasolutions.com>
2023-03-16 00:21:03 +0500

    -added height and width in place of 16:9 ratio

--- app/jobs/convert_portrait_to_landscape_job.rb
+++ app/jobs/convert_portrait_to_landscape_job.rb
@@ -9,16 +9,8 @@ class ConvertPortraitToLandscapeJob < ApplicationJob
         height = video_data&.imageheight
         width = video_data&.imagewidth
         if video_data&.imageheight > video_data&.imagewidth || (video_data&.rotation == 90 || video_data&.rotation == 270) 
-          gcd = greatest_common_divisor(height,width)
-          if video_data&.imageheight > video_data&.imagewidth
-            height_ratio = height/gcd
-            width_ratio = width/gcd
-          else
-            height_ratio = width/gcd
-            width_ratio = height/gcd
-          end
           temp_file = "#{Rails.root}/testing#{index}.mp4"
-          system(`ffmpeg -i "#{video_path}" -filter_complex "[0:v]scale=ih*#{height_ratio}/#{width_ratio}:-1,boxblur=luma_radius='min(h\,w)/20':luma_power=1:chroma_radius='min(cw\,ch)/20':chroma_power=1[bg];[bg][0:v]overlay=(W-w)/2:(H-h)/2,crop=h=iw*#{width_ratio}/#{height_ratio}" "testing#{index}.mp4"`)
+          system(`ffmpeg -i "#{video_path}" -filter_complex "[0:v]scale=ih*#{height}/#{width}:-1,boxblur=luma_radius='min(h\,w)/20':luma_power=1:chroma_radius='min(cw\,ch)/20':chroma_power=1[bg];[bg][0:v]overlay=(W-w)/2:(H-h)/2,crop=h=iw*#{width}/#{height}" "testing#{index}.mp4"`)
           if (File.exists?(temp_file))
             downloaded_video = open(temp_file)
             order.videos.attach(io: downloaded_video  , filename: "#{order&.videos[index]&.filename}")
@@ -29,21 +21,5 @@ class ConvertPortraitToLandscapeJob < ApplicationJob
         end
       end
     end
-  end
-  def greatest_common_divisor(height,width)
-    if (height > width) 
-      x = height;
-      y = width;
-    else 
-      x = width;
-      y = height;
-    end
-    rem = x % y;
-    while (rem != 0) 
-      x = y;
-      y = rem;
-      rem = x % y;
-    end
-    return y
   end
 end
