631cdc6012487e5bd6ddbeeec59fa1362a1b4547
Noor Fatima <noor.fatima@phaedrasolutions.com>
2023-03-27 21:11:44 +0500

    - added 16/9 as scale

--- app/jobs/convert_portrait_to_landscape_job.rb
+++ app/jobs/convert_portrait_to_landscape_job.rb
@@ -10,7 +10,7 @@ class ConvertPortraitToLandscapeJob < ActiveJob::Base
         width = video_data&.imagewidth
         if video_data&.imageheight > video_data&.imagewidth || (video_data&.rotation == 90 || video_data&.rotation == 270) 
           temp_file = "#{Rails.root}/testing#{index}.mp4"
-          system(`ffmpeg -i "#{video_path}" -filter_complex "[0:v]scale=ih*#{height}/#{width}:-1,boxblur=luma_radius='min(h\,w)/20':luma_power=1:chroma_radius='min(cw\,ch)/20':chroma_power=1[bg];[bg][0:v]overlay=(W-w)/2:(H-h)/2,crop=h=iw*9/16" "testing#{index}.mp4"`)
+          system(`ffmpeg -i "#{video_path}" -filter_complex "[0:v]scale=ih*16/9:-1,boxblur=luma_radius='min(h\,w)/20':luma_power=1:chroma_radius='min(cw\,ch)/20':chroma_power=1[bg];[bg][0:v]overlay=(W-w)/2:(H-h)/2,crop=h=iw*9/16" "testing#{index}.mp4"`)
           if (File.exists?(temp_file))
             downloaded_video = open(temp_file)
             order.videos.attach(io: downloaded_video  , filename: "#{order&.videos[index]&.filename}")
