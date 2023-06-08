34c9a29db7a8a25b6daf835cc5ae940a59ec7b92
Noor Fatima <noor.fatima@phaedrasolutions.com>
2023-06-08 21:20:08 +0500

    - showing data on console for 6" screen

--- app/jobs/asset_upload_job.rb
+++ app/jobs/asset_upload_job.rb
@@ -5,6 +5,10 @@ class AssetUploadJob < ApplicationJob
     if order.save!
       asset = order.file_type == 'image' ? order&.images : order&.videos
       asset_count = asset&.count
+      puts "asset_job"
+      puts order
+      puts asset_count
+      puts "=========="
       Delayed::Job.enqueue ConvertPortraitToLandscapeJob.new(order,asset_count) if order.present? && asset_count > 0
     end
   end
