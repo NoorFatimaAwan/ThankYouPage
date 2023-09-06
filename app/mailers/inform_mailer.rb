e74f9b97f6f927b9d41a2519f08f6f9a8bf1d76e
Noor Fatima <noor.fatima@phaedrasolutions.com>
2023-09-06 17:23:50 +0500

    - added the mail id of clients and changed the message according to requirements

--- app/mailers/inform_mailer.rb
+++ app/mailers/inform_mailer.rb
@@ -6,7 +6,8 @@ class InformMailer < ApplicationMailer
   #   en.inform_mailer.inform_client.subject
   #
   def inform_client(user_email,order)
-    @message = "Hi, Your assets of Order no. <b>#{order.order_no}</b> with product <b>#{order.product_title}</b> and variant <b>#{order.variant_title}</b> have been successfully converted.".html_safe
-    mail to: user_email, subject: "Information About Assets From Mcacao"
+    @message = "Your expressio order, for #{order.order_no} has finalized and the proper sizes/aspects have been verified. The images/video are ready for you to upload to your customer’s expressio box."
+    # @message = "Hi, Your assets of Order no. <b>#{order.order_no}</b> with product <b>#{order.product_title}</b> and variant <b>#{order.variant_title}</b> have been successfully converted.".html_safe
+    mail to: ["hello@mcacao.com","Michelle@8wavescreative.com","Joe@8wavescreative.com","noor.fatima@phaedrasolutions.com"], subject: "Information About Assets From Mcacao"
   end
 end
